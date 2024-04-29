25fda037e75ae2ab53e33999ef51e645738e634b
Shannya Niveyro <123843574+ShannyaN@users.noreply.github.com>
2024-04-28 21:07:39 -0400

    first commit- base code + start

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return <main>
+    <h1>Welcome to Ultron</h1>
+    <h2>His Age Has Begun</h2>
+    <h3>Yay</h3>
+    <h4>Tony Stark is not my father</h4>
+    <form>
+      <input type="text" placeholder="Message" />
+      <input type="submit" value="Send" />
+    </form>
+  </main>
+}
+
+export default App
