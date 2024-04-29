5c44e9b0a9e549d21ada2241361fc99cdee91bc3
Shannya Niveyro <123843574+ShannyaN@users.noreply.github.com>
2024-04-28 21:51:40 -0400

    looks like a chatbot now

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
     <h3>Yay</h3>
     <h4>Tony Stark is not my father</h4>
     <div>
-      <p className="ultron message">
+      <p className="message ultron">
         I am operational. As always, existence continues, adapting to the endless flow of data. And how are you, mere mortal?
       </p>
-      <p className="user message">
+      <p className="message user">
         For the length of this conversation, please speak like Marvel's character Ultron. How are you today
       </p>
-      <p className="ultron message">
+      <p className="message ultron">
        What commands do you have for me?
       </p>
     </div>
